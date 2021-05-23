3a5f74ed51436716efa99604a751ebcb3920c713
jakebasel <83322584+jakebasel@users.noreply.github.com>
2021-05-23 11:25:48 -0700

    Add files via upload

---
+++ 05. Competency Report/script.js
@@ -0,0 +1,53 @@
+var characterPoints = 100;
+var enemyPoints = 100;
+var attackPoints = 10;
+display();
+enemyDisplay();
+
+//main character functions
+function display() {
+    document.getElementById("character-points").innerHTML = `<b>Points: </b> ${characterPoints}`;
+    document.getElementById(`status`).innerHTML = '<h2>Playing...</h2>'
+}
+
+function attack() {
+    var newPoints = enemyPoints - attackPoints;
+    enemyPoints = newPoints;
+    enemyDisplay();
+    if (enemyPoints <= 0) {
+        document.getElementById(`attack-btn`).style.display = "none";
+        document.getElementById(`enemy-attack-btn`).style.display = "none";
+        document.getElementById(`status`).innerHTML = '<h2>GAME OVER... TOAD WON</h2> <button onclick="restart();">Restart</button>'
+    }
+}
+
+function restart() {
+    characterPoints = 100;
+    enemyPoints=100;
+    display();
+    enemyDisplay();
+    document.getElementById(`attack-btn`).style.display = "block";
+    document.getElementById(`enemy-attack-btn`).style.display = "block";
+}
+
+//enemy character functions
+function enemyDisplay() {
+    document.getElementById("enemy-points").innerHTML = `<b>Points: </b> ${enemyPoints}`;
+}
+
+function enemyAttack() {
+    var newPoints = characterPoints - attackPoints;
+    characterPoints = newPoints;
+    display();
+    if (characterPoints <= 0) {
+        document.getElementById(`attack-btn`).style.display = "none";
+        document.getElementById(`enemy-attack-btn`).style.display = "none";
+        document.getElementById(`status`).innerHTML = '<h2>GAME OVER... LINK WON</h2> <button onclick="restart();">Restart</button>'
+    }
+}
+
+function enemyRestart() {
+    characterPoints = 100;
+    display();
+    document.getElementById(`attack-btn`).style.display = "block";
+}
